8eed189a8d9aba3d15ea21eb570477caf2b09110
AndrewJBateman <gomezbateman@yahoo.com>
2022-01-15 20:55:26 +0100

    chore: add status popups ngx-toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { AppComponent } from './app.component';
 import { CreateProjectComponent } from './components/create-project/create-project.component';
 import { ListProjectsComponent } from './components/list-projects/list-projects.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { MaterialModule } from './material/material.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, CreateProjectComponent, ListProjectsComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     ReactiveFormsModule,
     FlexLayoutModule,
     MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent],
